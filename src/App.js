b43d50277fd40f1b373062cb986f3d02911b3434
sidchopp <sidchopp@gmail.com>
2024-05-08 14:17:04 -0600

    Routesfor Login,Signup,Home and Navbar created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import { Navbar } from "./components/navbar/Navbar";
+
 function App() {
-  return <div className="App">Finance tracker</div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
