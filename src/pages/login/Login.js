8c9aec01ade718bac85a4a949b7f91bc44b48e6b
sidchopp <sidchopp@gmail.com>
2024-05-13 17:28:55 -0600

    Created and used useLogin hook

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,22 +1,22 @@
 import { useState } from "react";
+import { useLogin } from "../../hooks/useLogin";
 import styles from "./Login.module.css";
-
-import React from "react";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, error, isPending } = useLogin();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(email, password);
+    login(email, password);
   };
 
   return (
     <form onSubmit={handleSubmit} className={styles["login-form"]}>
-      <h2>Login</h2>
+      <h2>login</h2>
       <label>
-        <span>Email:</span>
+        <span>email:</span>
         <input
           type="email"
           onChange={(e) => setEmail(e.target.value)}
@@ -24,14 +24,20 @@ const Login = () => {
         />
       </label>
       <label>
-        <span>Password:</span>
+        <span>password:</span>
         <input
           type="password"
           onChange={(e) => setPassword(e.target.value)}
           value={password}
         />
       </label>
-      <button className="btn">Login</button>
+      {!isPending && <button className="btn">Login</button>}
+      {isPending && (
+        <button className="btn" disabled>
+          Loading
+        </button>
+      )}
+      {error && <p>{error}</p>}
     </form>
   );
 };
