a9cd873d11bb1028ee92879c5284b941a25e37ed
sidchopp <sidchopp@gmail.com>
2024-05-13 16:30:05 -0600

    Created useLogout hook, dispatch for LOGOUT action

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { projectAuth } from "../firebase/config";
+import { useAuthContext } from "./useAuthContext";
+
+export const useLogout = () => {
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
+
+  const logout = async () => {
+    setError(null);
+    setIsPending(true);
+
+    try {
+      //sign out the User
+      await projectAuth.signOut();
+
+      // Dispatch LOGOUT action for Reducer
+      dispatch({ type: "LOGOUT" }); // No need for payload here as we are not updating the state
+
+      setIsPending(false);
+      setError(null);
+    } catch (err) {
+      console.log(err.message);
+      setError(err.message);
+      setIsPending(false);
+    }
+  };
+
+  return { error, isPending, logout };
+};
