d936a96d59d97bc40386695aba46fa55ff1ac00b
sidchopp <sidchopp@gmail.com>
2024-05-13 15:45:56 -0600

    Dispatch a LOGIN action

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -4,6 +4,8 @@ export const AuthContext = createContext();
 
 export const authReducer = (state, action) => {
   switch (action.type) {
+    case "LOGIN":
+      return { ...state, user: action.payload };
     default:
       return state;
   }
@@ -13,6 +15,7 @@ export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
   });
+
   return (
     <AuthContext.Provider value={{ ...state, dispatch }}>
       {children}
