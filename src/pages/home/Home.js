9626797a09e724a7c817e911304bd42f604cc2b2
sidchopp <sidchopp@gmail.com>
2024-05-14 13:36:07 -0600

    TransactionForm RC created for Home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,15 @@
 import styles from "./Home.module.css";
+import { TransactionForm } from "./Transaction";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>My Transactions</div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
